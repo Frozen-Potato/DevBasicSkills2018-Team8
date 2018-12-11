2fec03d9f6f8fd5705a13f3be49b82081e38b5dc
McFkCoding <Mcfkcoding@gmail.com>
2018-12-11 12:12:33 +0200

    fixed additional files

--- AdditionalFile/generate2.py
+++ AdditionalFile/generate2.py
@@ -1,5 +1,5 @@
 f = open("timer.txt","w+")
 start = 1950
 for i in range(65):
-        start += 75
+        start += 90
         f.write(".container2 svg tspan > tspan:nth-of-type(" + str(i+12) + ") {\n\t animation: draw 300ms " + str(start) + "ms forwards;\n }\n")
